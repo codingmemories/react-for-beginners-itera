59f2a1b0f92cb09a8d76c2b5535f0781eb9afd5e
codingmemories <vasilisapj89@gmail.com>
2022-08-10 01:17:42 +0300

    add content

---
+++ my-page/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import DATA from "./store/data.json";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {DATA.map((datas) => {
+        return (
+          <div className="container">
+            <h1>About me:</h1>
+            <section>
+              <h2>{datas["first name"]}</h2>
+              <p>{datas["short biography"]}</p>
+            </section>
+            <section>
+              <h2>My contacts:</h2>
+              <p>{`📞 phone: ${datas["public contacts"].phone}, 📩email: ${datas["public contacts"].email}`}</p>
+            </section>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
